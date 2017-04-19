07c04052a469e352593a1806cd9e2f80de41bb53
maulana <maulana.robert.mr@gmail.com>
2017-04-19 17:23:03 +0700

    refactor store

---
+++ client/src/store/modules/mutations.js
@@ -0,0 +1,14 @@
+export const Mutations = {
+  dataHouse: (state, payload) => {
+    state.house = payload
+  },
+  newHouse: (state, payload) => {
+    state.house = payload
+  },
+  removeHouse: (state, payload) => {
+    state.house = payload
+  },
+  updateHouse: (state, payload) => {
+    state.house = payload
+  }
+}
